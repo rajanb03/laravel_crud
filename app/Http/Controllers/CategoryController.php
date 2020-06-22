70cd1c8a89925909a4abd56061d00aada4477302
rajanb03 <ribarot03@gmail.com>
2020-06-22 20:32:06 +0530

    Crud of Category Using PSR Standards

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,27 +9,25 @@ class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
         $data = Category::all();
-        // return view('show_cat')->with('data',$data);
         return response()->json($data);
     }
+
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function create()
     {
         return view('create_cat');
     }
+
     /**
      * Store a newly created resource in storage.
-     *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
@@ -44,20 +42,21 @@ class CategoryController extends Controller
        
        return response()->json(['name' => $request->name, 'description' => $request->description]);
     }
+
     /**
      * Display the specified resource.
-     *
+     * @param Integer $id
      * @return \Illuminate\Http\Response
      */
     public function show($id) 
     {
         $data = Category::find($id);
-        // return view('read_cat')->with('data',$data);
         return response()->json($data);
     }
+
     /**
      * Show the form for editing the specified resource.
-     *
+     * @param Integer $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -65,31 +64,31 @@ class CategoryController extends Controller
         $data = Category::find($id);
         return response()->json($data);
     }
+
     /**
      * Update the specified resource in storage.
-     *
+     * @param Integer $id
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        $request->validate 
-        ([
+        $request->validate ([
             'name' => 'required|unique:category|max:255',
             'description' => 'required',
         ]);
 
-        Category::where('id', $id)->update
-        ([
+        Category::where('id', $id)->update ([
             'name' => $request->name,
             'description' => $request->description
         ]);
 
          return response()->json(['name' => $request->name, 'description' => $request->description]);
     }
+
     /**
      * Remove the specified resource from storage.
-     *
+     * @param Integer $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
