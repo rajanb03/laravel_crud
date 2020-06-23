68e2fa5e38c671e7600843218d797cf360fa40f6
rajanb03 <ribarot03@gmail.com>
2020-06-23 09:49:56 +0530

    Revert "Crud of Category With PSR Standards"
    This reverts commit e222856ff6e37342aab75e7234cd1a529ef4a1c0.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,37 +5,39 @@ namespace App\Http\Controllers;
 use App\Category;
 use Illuminate\Http\Request;
 
-class CategoryController extends Controller 
-{
+class CategoryController extends Controller {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+
+    public function index() {
         $data = Category::all();
+
         // return view('show_cat')->with('data',$data);
         return response()->json($data);
     }
+
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
+
+    public function create() {
         return view('create_cat');
     }
+
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        $request->validate ([
+    
+    public function store(Request $request) {
+        $validatedData = $request->validate ([
             'name' => 'required|unique:category|max:255',
             'description' => 'required',
         ]);
@@ -44,57 +46,66 @@ class CategoryController extends Controller
        
        return response()->json(['name' => $request->name, 'description' => $request->description]);
     }
+
     /**
      * Display the specified resource.
      *
+     * @param  \App\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function show($id) 
-    {
+
+    public function show($id) {
         $data = Category::find($id);
+
         // return view('read_cat')->with('data',$data);
         return response()->json($data);
     }
+
     /**
      * Show the form for editing the specified resource.
      *
+     * @param  \App\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
+
+    public function edit($id) {
         $data = Category::find($id);
+
         return response()->json($data);
     }
+
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        $request->validate 
-        ([
+
+    public function update(Request $request, $id) {
+        $validatedData = $request->validate ([
             'name' => 'required|unique:category|max:255',
             'description' => 'required',
         ]);
 
-        Category::where('id', $id)->update
-        ([
+        Category::where('id',$id)->update([
             'name' => $request->name,
             'description' => $request->description
         ]);
 
          return response()->json(['name' => $request->name, 'description' => $request->description]);
     }
+
     /**
      * Remove the specified resource from storage.
      *
+     * @param  \App\Category  $category
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    {
+
+    public function destroy($id) {
         Category::where('id', $id)->delete();
+        
         return response()->json(['id' => $id]);
     }
 }
