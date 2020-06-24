f0f822b3ec1774937ee0587714a5ae7e129c1367
rajanb03 <ribarot03@gmail.com>
2020-06-24 16:11:58 +0530

    Add getRecords() for getting category to download excel file

--- app/Category.php
+++ app/Category.php
@@ -3,10 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Category extends Model
 {
     protected $table = "categories";
 
     protected $fillable = ['name','description',]; 
+     
+    public static function getRecords()
+    {
+     	$records = DB::table('categories')->select('name','description')->orderBy('id', 'asc')->get()->toArray();
+
+     	return $records;
+   }
 }
