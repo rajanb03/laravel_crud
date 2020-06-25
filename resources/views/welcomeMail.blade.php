0f5306ef32cec0db7017ff67a585426190d849b8
rajanb03 <ribarot03@gmail.com>
2020-06-25 18:44:22 +0530

    Welcome Message View

---
+++ resources/views/welcomeMail.blade.php
@@ -0,0 +1,11 @@
+<!DOCKTYPE html>
+<html>
+    <body>
+        <div>
+                <h1> Welcome to the site. </h1>
+
+                <span> Thank You,<br>
+                       From Laravel.</span>
+        </div>
+    </body>
+</html>
