b850011341b6729735090404e2ee2cca6a44c0bd
rajanb03 <ribarot03@gmail.com>
2020-07-01 17:00:06 +0530

    For Sending Welcome Mail

---
+++ app/Listeners/WelcomeListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserRegistered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Mail\WelcomeMessage;
+use App\Jobs\WelcomeEmailJob;
+use Illuminate\Support\Facades\Mail;
+
+class WelcomeListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserRegistered  $event
+     * @return void
+     */
+    public function handle(UserRegistered $event)
+    {
+        $user = $event->user;
+        Mail::to($user->email)->send(new WelcomeMessage());
+    }
+}
